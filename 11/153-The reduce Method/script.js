63e3c6e41fe0371b3eb43427d5b83397338b486b
thuannd <thuansmile2479@gmail.com>
2023-02-03 15:44:25 +0700

    153-The reduce Method

---
+++ 11/153-The reduce Method/script.js
@@ -0,0 +1,117 @@
+'use strict';
+//data
+const account1 = {
+    owner: 'Jonas Schemdtmann',
+    movements: '[200, 450, -400, 3000, -650, -130, 70, 1300]',
+    interestRate: 1.2,
+    pin: 1111,
+};
+
+const account2 = {
+    owner: 'Jessica Davis',
+    movements: '[5000, 3400, -150, -790, -3210, -1000, 8500, -30]',
+    interestRate: 1.5,
+    pin: 2222,
+}
+
+const account3 = {
+    owner: 'Steven Thomas Williams',
+    movements: '[200, -200, 340, -300, -20, 50, 400, -460]',
+    interestRate: 0.7,
+    pin: 3333,
+}
+
+const account4 = {
+    owner: 'Sarah Smith',
+    movements: '[430, 1000, 700, 50, 90]',
+    interestRate: 1,
+    pin: 4444,
+}
+
+const accounts = [account1, account2, account3, account4 ];
+
+//element
+
+const labelWelcome = document.querySelector('.welcome');
+const labelDate = document.querySelector('.date');
+const labelBalance = document.querySelector('.balance__value');
+const labelSumIn = document.querySelector('.summary__value--in');
+const labelSumOut = document.querySelector('.summary__value--out');
+const labelSumInterest = document.querySelector('.summary__value--interest');
+const labelTimer = document.querySelector('.timer');
+
+const containerApp = document.querySelector('.app');
+const containerMovements = document.querySelector('.movements');
+
+const btnLogin = document.querySelector('.login__btn');
+const btnTransfer = document.querySelector('.form__btn--transfer');
+const btnLoan = document.querySelector('.form__btn--loan');
+const btnClose = document.querySelector('.form__btn--close');
+const btnSort = document.querySelector('.form__btn--sort');
+
+const inputLoginUsername = document.querySelector('.login__input--user');
+const inputLoginPin = document.querySelector('.login__input--pin');
+const inputTransferto = document.querySelector('.login__input--to');
+const inputTransferAmount = document.querySelector('.login__input--amount');
+const inputLoanAmount = document.querySelector('.login__input--loan--amount');
+const inputCloseUsername = document.querySelector('.form__input--user');
+const inputClosePin = document.querySelector('.login__input--pin');
+
+const displayMovements = function(movements){
+    containerMovements.innerHTML='';
+
+    movs.forEach(function (mov, i) {
+        const type = mov > 0 ? 'deposits' : 'withdrawal';
+
+        const html = `
+        <div class="movements__row">
+            <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
+            <div class="movements__value">${mov}</div>
+        </div>
+        ` ;
+
+        containerMovements.insertAdjacentHTML
+        ('afterbegin', html);
+
+    });
+};
+
+displayMovements(account1.movements);
+
+const calcDisplayBalance = function (movements){
+    const balance  = movements.reduce((acc, mov)=> acc 
+    + mov, 0);
+    labelBalance.textContent = `${balance} EUR`;
+};
+calcDisplayBalance(account1.movements);
+
+const createUsernames = function (accs) {
+    accs.forEach(function (acc){
+        acc.username = acc.owner
+        .toLowerCase()
+        .split('')
+        .map(name => name[0])
+        .join('');
+    });
+};
+createUsernames(accounts);
+// console.log(accounts)
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+console.log(movements);
+
+const balance = movements.reduce((acc, cur) => acc + 
+cur, 0);
+console.log(balance);
+
+let balance2 = 0;
+for(const mov of movements) balance2 += mov;
+console.log(balance2);
+
+//Maxium value
+const max = movements.reduce((acc, mov)=>{
+    if(acc > mov) return acc;
+    else return mov;
+},movements[0]);
+console.log(max)
