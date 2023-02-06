67e15df2af21065705a43965ddfd64cab99423b5
thuannd <thuansmile2479@gmail.com>
2023-02-07 00:32:50 +0700

    162. flat and flatMap

---
+++ 11/162. flat and flatMap/script.js
@@ -0,0 +1,15 @@
+const arr = [[1, 2, 3], [4, 5, 6], 7, 8];
+console.log(arr.flat());
+const arrDeep = [[[1, 2], 3], [4, [5, 6]], 7, 8];
+console.log(arrDeep.flat(2));
+
+const overalBalance = accounts
+  .map(acc => acc.movements)
+  .flat()
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(overalBalance);
+
+const overalBalance2 = accounts
+  .flatMap(acc => acc.movements)
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(overalBalance2);
