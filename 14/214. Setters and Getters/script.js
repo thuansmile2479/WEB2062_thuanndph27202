ff00c13d804763598d8f0de04397bb1d8cf75e0d
thuannd <thuansmile2479@gmail.com>
2023-02-11 00:26:02 +0700

    214. Setters and Getters

---
+++ 14/214. Setters and Getters/script.js
@@ -0,0 +1,36 @@
+class PersonCl {
+  constructor(fullName, birthYear) {
+    this.fullName = fullName;
+    this.birthYear = birthYear;
+  }
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  }
+  greet() {
+    console.log(`Hey ${this.fullName}`);
+  }
+  get age() {
+    return 2037 - this.birthYear;
+  }
+  set fullName(name) {
+    if (name.includes(' ')) this._fullName = name;
+    else alert(`${name} is not a full name!`);
+  }
+  get fullName() {
+    return this._fullName;
+  }
+  // Static method
+  static hey() {
+    console.log('Hey there 👋');
+    console.log(this);
+  }
+}
+const jessica = new PersonCl('Jessica Davis', 1996);
+console.log(jessica);
+
+jessica.calcAge();
+
+console.log(jessica.age);
+console.log(jessica.__proto__ === PersonCl.prototype);
+
+jessica.greet();
